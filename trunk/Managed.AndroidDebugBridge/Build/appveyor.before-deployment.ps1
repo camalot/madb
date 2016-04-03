e8d7264473157b8436a1a0b2e22be983fcc62393
camalot <camalot@gmail.com>
2016-04-03 16:05:38 -0500

    [deploy:draft] updates to appveyor scripts

--- trunk/Managed.AndroidDebugBridge/Build/appveyor.before-deployment.ps1
+++ trunk/Managed.AndroidDebugBridge/Build/appveyor.before-deployment.ps1
@@ -3,7 +3,7 @@
  #
  #>
 
-$commitMessageRegex = "^\[deploy\:(pre-release|draft|release)\]$";
+$commitMessageRegex = "^\[deploy\:(pre-release|draft|release)\]";
 
 # Must come from master branch.
 # Must not have a PULL Request Number
@@ -15,6 +15,17 @@ if ( !$env:APPVEYOR_PULL_REQUEST_NUMBER -and ($env:APPVEYOR_REPO_BRANCH -eq "mas
   $env:CI_DEPLOY_FTP = $true;
 	$env:CI_DEPLOY_WebHook = $true;
 	$env:CI_DEPLOY_WebDeploy = $true;
+
+	$env:CI_DEPLOY_GITHUB_ISDRAFT = "false";
+	$env:CI_DEPLOY_GITHUB_ISPRERELEASE = "false";
+
+	if($matches[1] -ieq "draft") {
+		$env:CI_DEPLOY_GITHUB_ISDRAFT = "true";
+	}
+	if($matches[1] -ieq "pre-release") {
+		$env:CI_DEPLOY_GITHUB_ISDRAFT = "false";
+	}
+
 } else {
 	# Do not assign a release number or deploy
   $env:CI_DEPLOY_NUGET = $false;
