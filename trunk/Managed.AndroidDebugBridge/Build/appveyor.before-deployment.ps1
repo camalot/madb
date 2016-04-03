8f763ca4051f5e1b718cc0dc7016af2475bc260b
camalot <camalot@gmail.com>
2016-04-03 16:32:34 -0500

    [deploy:draft] update what triggers a deploy

--- trunk/Managed.AndroidDebugBridge/Build/appveyor.before-deployment.ps1
+++ trunk/Managed.AndroidDebugBridge/Build/appveyor.before-deployment.ps1
@@ -8,7 +8,7 @@ $commitMessageRegex = "^\[deploy\:(pre-release|draft|release)\]";
 # Must come from master branch.
 # Must not have a PULL Request Number
 # Must match regex
-if ( !$env:APPVEYOR_PULL_REQUEST_NUMBER -and ($env:APPVEYOR_REPO_BRANCH -eq "master") -and ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match $commitMessageRegex) ) {
+if ( ($env:APPVEYOR_REPO_BRANCH -eq "master") -and ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match $commitMessageRegex) ) {
 	$env:CI_RELEASE_DESCRIPTION = $env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED
 	$env:CI_DEPLOY_NUGET = $true;
   $env:CI_DEPLOY_GITHUB = $true;
