b5fcbf435f140f2c902bb9bd6d678d5009778c94
camalot <camalot@gmail.com>
2016-04-03 16:44:07 -0500

    [deploy:draft] trying to get this to do the deploy now

--- trunk/Managed.AndroidDebugBridge/Build/appveyor.before-deployment.ps1
+++ trunk/Managed.AndroidDebugBridge/Build/appveyor.before-deployment.ps1
@@ -8,6 +8,11 @@ $commitMessageRegex = "^\[deploy\:(pre-release|draft|release)\]";
 # Must come from master branch.
 # Must not have a PULL Request Number
 # Must match regex
+
+
+Write-Host "$env:APPVEYOR_REPO_COMMIT_MESSAGE -match $commitMessageRegex";
+Write-Host "Branch: $env:APPVEYOR_REPO_BRANCH";
+
 if ( ($env:APPVEYOR_REPO_BRANCH -eq "master") -and ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match $commitMessageRegex) ) {
 	$env:CI_RELEASE_DESCRIPTION = $env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED
 	$env:CI_DEPLOY_NUGET = $true;
